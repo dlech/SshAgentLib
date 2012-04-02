b3dea1eae184355b9630bf2cfab75919665369a1
dlech <gismho@gmail.com>
2012-04-02 00:20:19 -0500

    got basic .ppk file reading working

---
+++ PageantSharp/PpkFile.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace dlech.PageantSharp
+{
+	/// <summary>
+	/// Used to read PuTTY Private Key (.ppk) files
+	/// </summary>
+	public class PpkFile
+	{
+		private char[] delimeters = { ':' };
+		private const string puttyUserKeyFile2Key = "PuTTY-User-Key-File-2";
+		private const string encryptionKey = "Encryption";
+		private const string commentKey = "Comment";
+		private const string publicLinesKey = "Public-Lines";
+		private const string privateLinesKey = "Private-Lines";
+		private const string privateMACKey = "Private-MAC";
+
+		public string KeyType { get; private set; }
+		public string Encryption { get; private set; }
+		public string Comment { get; private set; }
+		public string PublicKey { get; private set; }
+		public string PrivateKey { get; private set; }
+		public string PrivateMAC { get; private set; }
+
+
+		/// <summary>
+		/// Creates new instance of PpkFile from data array
+		/// </summary>
+		/// <param name="data">bytes read from file</param>
+		public PpkFile(byte[] data)
+		{
+			ProcessData(data);
+		}
+
+		/// <summary>
+		/// Creates new instance of PpkFile from specifed file
+		/// </summary>
+		/// <param name="fileName">the name of the file to open</param>
+		/// <exception cref="System.ArgumentException">see <see cref="System.IO.File.OpenRead(string)"/></exception>
+		/// <exception cref="System.ArgumentNullException">see <see cref="System.IO.File.OpenRead(string)"/></exception>
+		/// <exception cref="System.IO.PathTooLongException">see <see cref="System.IO.File.OpenRead(string)"/></exception>
+		/// <exception cref="System.IO.DirectoryNotFoundException">see <see cref="System.IO.File.OpenRead(string)"/></exception>
+		/// <exception cref="System.UnauthorizedAccessException">see <see cref="System.IO.File.OpenRead(string)"/></exception>
+		/// <exception cref="System.IO.FileNotFoundException">see <see cref="System.IO.File.OpenRead(string)"/></exception>
+		/// <exception cref="System.NotSupportedException">see <see cref="System.IO.File.OpenRead(string)"/></exception>
+		public PpkFile(string fileName)
+		{
+			FileStream stream = File.OpenRead(fileName);
+			byte[] buffer = new byte[stream.Length];
+			stream.Read(buffer, 0, buffer.Length);
+			ProcessData(buffer);
+		}
+
+		private void ProcessData(byte[] data)
+		{
+			string line;
+			string[] pair = new string[2];
+			int lineCount, i;
+
+			Stream stream = new MemoryStream(data);
+			StreamReader reader = new StreamReader(stream);
+
+			line = reader.ReadLine();
+			pair = line.Split(delimeters, 2);
+			if (pair[0] != puttyUserKeyFile2Key) {
+				throw new PpkFileFormatException();
+			}
+			this.KeyType = pair[1].Trim();
+
+			line = reader.ReadLine();
+			pair = line.Split(delimeters, 2);
+			if (pair[0] != encryptionKey) {
+				throw new PpkFileFormatException();
+			}
+			this.Encryption = pair[1].Trim();
+
+			line = reader.ReadLine();
+			pair = line.Split(delimeters, 2);
+			if (pair[0] != commentKey) {
+				throw new PpkFileFormatException();
+			}
+			this.Comment = pair[1].Trim();
+
+			line = reader.ReadLine();
+			pair = line.Split(delimeters, 2);
+			if (pair[0] != publicLinesKey) {
+				throw new PpkFileFormatException();
+			}
+			lineCount = int.Parse(pair[1]);
+			this.PublicKey = string.Empty;
+			for (i = 0; i < lineCount; i++) {
+				this.PublicKey += reader.ReadLine();
+			}
+
+			line = reader.ReadLine();
+			pair = line.Split(delimeters, 2);
+			if (pair[0] != privateLinesKey) {
+				throw new PpkFileFormatException();
+			}
+			lineCount = int.Parse(pair[1]);
+			this.PrivateKey = string.Empty;
+			for (i = 0; i < lineCount; i++) {
+				this.PrivateKey += reader.ReadLine();
+			}
+
+			line = reader.ReadLine();
+			pair = line.Split(delimeters, 2);
+			if (pair[0] != privateMACKey) {
+				throw new PpkFileFormatException();
+			}
+			this.PrivateMAC = pair[1].Trim();
+		}
+	}
+
+
+}
+
---
+++ PageantSharpTestProject/PpkFileTest.cs
@@ -0,0 +1,115 @@
+﻿using dlech.PageantSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+using System.Diagnostics;
+using System.IO;
+using PageantSharpTestProject.Properties;
+
+namespace PageantSharpTestProject
+{
+
+
+	/// <summary>
+	///This is a test class for PpkFileTest and is intended
+	///to contain all PpkFileTest Unit Tests
+	///</summary>
+	[TestClass()]
+	public class PpkFileTest
+	{
+
+
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
+
+		#region Additional test attributes
+		// 
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//public static void MyClassInitialize(TestContext testContext)
+		//{
+		//}
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//public static void MyClassCleanup()
+		//{
+		//}
+		//
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//public void MyTestInitialize()
+		//{
+		//}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+		#endregion
+
+
+		/// <summary>
+		///A test for PpkFile Constructor
+		///</summary>
+		[TestMethod()]
+		public void PpkFileConstructorFromFileTest()
+		{
+			Assembly asm = Assembly.GetExecutingAssembly();
+			string dir = Path.GetDirectoryName(asm.Location);
+			string fileName = Path.Combine(dir, @"Resources\withoutPassphrase.ppk");
+			try {
+				PpkFile target = new PpkFile(fileName);
+				Assert.AreEqual("ssh-rsa", target.KeyType);
+				Assert.AreEqual("none", target.Encryption);
+				Assert.AreEqual("without passphrase", target.Comment);
+				Assert.AreEqual("AAAAB3NzaC1yc2EAAAABJQAAAIEAqtfJwYLL9N6UyMYIrYoGu9eEZCIT3pS5OI0V" +
+											  "4t80baJDXPkdUBqkokcHoDjXKOy620c6MmFROBZ6AZHRvlGztefIT2+oVGJxR3TR" +
+									      "dPmQhhPzgyvsdWAzjQBIj7rZz5Dzu/sDOa2wm5PRHSMrk7G4f2b2/uaGuUvC+Ga5" +
+												"aKXEDnc=", target.PublicKey);
+				Assert.AreEqual("AAAAgHyrTgnAT6TZxoSsL9iVJ4InpcyHkfVzcmeJjIL15/z53iFAKiWyk9BdWJeD" +
+											  "bJN8UQDg8x3YUAZVl01A5SoERN18aB7lgajejzJgQFOP5Ad7vA/83dFbrzAf10Ah" +
+											  "dbtaqHUdMwMWqdkqVa2EYCZ+O+0PeewHA3uBJECHlP/NN+GdAAAAQQDtQNlZb5AD" +
+											  "d9RthpU1X6+ePcR7POnz01GrUPRARzRB2h5JP+mwFnfjANSKhZhrzePpIL1jKYyI" +
+											  "o12b1qV/IXY5AAAAQQC4V5eZV62MzOuf1kdUVysFCVzt3mMLLcn57RQwRpqQfp5j" +
+											  "0r2JNiAFBYo4k/9phYYJ0FziDIz/MEvYMwXLCiovAAAAQQDMYEQojQraSZDbcUwy" +
+											  "OaEtSNGh9qtYIPuYilRFbiIU55Az5iujw8c7LCpNycSGeo6GGLAt6VCjp8v8abb0" +
+												"wOqJ", target.PrivateKey);
+				Assert.AreEqual("f7c9bf63097216304a05c1426ac9d42c4b3825cd", target.PrivateMAC);
+			} catch (Exception ex) {
+				Assert.Fail(ex.ToString());
+			}
+
+		}
+
+		/// <summary>
+		///A test for PpkFile Constructor
+		///</summary>
+		[TestMethod()]
+		public void PpkFileConstructorFromDataTest()
+		{
+			byte[] data = Resources.withoutPassphrase_ppk;
+			PpkFile target = new PpkFile(data);
+
+		}
+	}
+}
