e0037fbf3f70cdb1f0fdbbefb2bee41f3a8a10bf
David Lechner <david@lechnology.com>
2013-07-08 21:13:58 -0500

    ppk uses codepage 1252 encoding.
    fixes dlech/KeeAgent#57

--- SshAgentLib/PpkFormatter.cs
+++ SshAgentLib/PpkFormatter.cs
@@ -214,7 +214,7 @@ namespace dlech.SshAgentLib
       string[] pair = new string[2];
       int lineCount, i;
 
-      StreamReader reader = new StreamReader(aStream);
+      StreamReader reader = new StreamReader(aStream, Encoding.GetEncoding(1252));
       char[] delimArray = { cDelimeter };
 
       try {
@@ -424,7 +424,7 @@ namespace dlech.SshAgentLib
       if (fileData.ppkFileVersion != Version.V1) {
         builder.AddStringBlob(fileData.publicKeyAlgorithm.GetIdentifierString());
         builder.AddStringBlob(fileData.privateKeyAlgorithm.GetIdentifierString());
-        builder.AddStringBlob(fileData.comment);
+        builder.AddBlob(Encoding.GetEncoding(1252).GetBytes(fileData.comment));
         builder.AddBlob(fileData.publicKeyBlob);
         builder.AddInt(fileData.privateKeyBlob.Data.Length);
       }
