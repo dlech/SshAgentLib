6fa8f01a49819d618fba63012929cc9167a9c4f8
David Lechner <david@lechnology.com>
2015-07-10 22:28:41 -0500

    Add a KeyPicker dialog.
    This is intended to be used by the KeyFilterCallback.
    
    Issue dlech/KeeAgent#38

---
+++ Ui/WinForms/KeyPicker.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace dlech.SshAgentLib.WinForms
+{
+  partial class KeyPicker
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.selectButton = new System.Windows.Forms.Button();
+      this.cancelButton = new System.Windows.Forms.Button();
+      this.keyDataGridView = new System.Windows.Forms.DataGridView();
+      ((System.ComponentModel.ISupportInitialize)(this.keyDataGridView)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // selectButton
+      // 
+      this.selectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.selectButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+      this.selectButton.Location = new System.Drawing.Point(498, 201);
+      this.selectButton.Name = "selectButton";
+      this.selectButton.Size = new System.Drawing.Size(75, 23);
+      this.selectButton.TabIndex = 1;
+      this.selectButton.Text = "&Select";
+      this.selectButton.UseVisualStyleBackColor = true;
+      // 
+      // cancelButton
+      // 
+      this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.cancelButton.Location = new System.Drawing.Point(579, 201);
+      this.cancelButton.Name = "cancelButton";
+      this.cancelButton.Size = new System.Drawing.Size(75, 23);
+      this.cancelButton.TabIndex = 2;
+      this.cancelButton.Text = "&Cancel";
+      this.cancelButton.UseVisualStyleBackColor = true;
+      // 
+      // keyDataGridView
+      // 
+      this.keyDataGridView.AllowUserToAddRows = false;
+      this.keyDataGridView.AllowUserToDeleteRows = false;
+      this.keyDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.keyDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.keyDataGridView.Location = new System.Drawing.Point(12, 12);
+      this.keyDataGridView.Name = "keyDataGridView";
+      this.keyDataGridView.ReadOnly = true;
+      this.keyDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+      this.keyDataGridView.Size = new System.Drawing.Size(642, 183);
+      this.keyDataGridView.TabIndex = 3;
+      // 
+      // KeyPicker
+      // 
+      this.AcceptButton = this.selectButton;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.CancelButton = this.cancelButton;
+      this.ClientSize = new System.Drawing.Size(666, 236);
+      this.Controls.Add(this.keyDataGridView);
+      this.Controls.Add(this.cancelButton);
+      this.Controls.Add(this.selectButton);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "KeyPicker";
+      this.ShowIcon = false;
+      this.ShowInTaskbar = false;
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+      this.Text = "Select a Key";
+      ((System.ComponentModel.ISupportInitialize)(this.keyDataGridView)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button selectButton;
+    private System.Windows.Forms.Button cancelButton;
+    private System.Windows.Forms.DataGridView keyDataGridView;
+  }
+}
---
+++ Ui/WinForms/KeyPicker.cs
@@ -0,0 +1,70 @@
+﻿//
+// KeyInfoView.cs
+//
+// Author(s): David Lechner <david@lechnology.com>
+//
+// Copyright (c) 2015 David Lechner
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dlech.SshAgentLib.WinForms
+{
+  public partial class KeyPicker : Form
+  {
+    public IEnumerable<ISshKey> SelectedKeys
+    {
+      get
+      {
+        return keyDataGridView.SelectedRows.Cast<DataGridViewRow>()
+          .Select(r => ((KeyWrapper)r.DataBoundItem).GetKey());
+      }
+    }
+
+    public KeyPicker(ICollection<ISshKey> keys)
+    {
+      if (keys == null) {
+        throw new ArgumentNullException("keys");
+      }
+      if (keys.Count == 0) {
+        throw new ArgumentException("No keys in list.", "keys");
+      }
+      InitializeComponent();
+      keyDataGridView.ColumnAdded += keyDataGridView_ColumnAdded;
+      keyDataGridView.DataSource = keys.Select(k => new KeyWrapper(k)).ToList();
+      keyDataGridView.Rows[0].Selected = true;
+    }
+
+    void keyDataGridView_ColumnAdded(object sender, DataGridViewColumnEventArgs e)
+    {
+      e.Column.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+      if (e.Column.DataPropertyName == "Confirm" || e.Column.DataPropertyName == "Lifetime") {
+        e.Column.Visible = false;
+      }
+    }
+  }
+}
