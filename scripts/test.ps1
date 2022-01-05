343cd59e779bdd30ada62158b7adc2bbdd74948d
David Lechner <david@lechnology.com>
2022-01-05 17:10:29 -0600

    nunit: update to latest version

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,3 +1,3 @@
 # build and run unit tests
 msbuild "SshAgentLibTests"
-vstest.console "SshAgentLibTests\bin\Debug\SshAgentLibTests.dll" /TestAdapterPath:"packages\NUnit3TestAdapter.3.7.0\tools"
+vstest.console "SshAgentLibTests\bin\Debug\SshAgentLibTests.dll" /TestAdapterPath:"packages\NUnit3TestAdapter.4.2.0\build\net35"
