d3ab9f881d44331ef46ddb1d4b778b444fce7468
David Lechner <david@lechnology.com>
2022-01-05 19:55:56 -0600

    scripts/test: exit early if command fails

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,3 +1,9 @@
 # build and run unit tests
 msbuild "SshAgentLibTests"
+if ($LastExitCode) {
+    exit $LastExitCode
+}
 vstest.console "SshAgentLibTests\bin\Debug\SshAgentLibTests.dll" /TestAdapterPath:"packages\NUnit3TestAdapter.4.2.0\build\net35"
+if ($LastExitCode) {
+    exit $LastExitCode
+}
