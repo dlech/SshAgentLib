dlech/SshAgentLib

commit 0980bee3caaa012ea6887bf4c324642f5089dffe

Agent: handle missed cases for non-seekable streams
Some agents use network streams that are not seekable. This adds tests
to exercise the agent methods with both seekable and non-seekable
streams. Test failures are fixed by copying the data from the stream
to a new MemoryStream and using that instead.

Fixes: https://github.com/dlech/SshAgentLib/issues/12

SshAgentLib/Agent.cs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2012-2015,2017-2018,2022 David Lechner <david@lechnology.com>
 // Author(s): David Lechner
 //            Max Laverse
@@ -440,6 +440,24 @@ namespace dlech.SshAgentLib
                 // which returns an error to the stream.
             }
 
+            // There are some parts of the code below that rely on knowing the
+            // position in the stream. So if a stream is not seekable, we need
+            // to read the full length now to a copy in memory.
+            if (!messageParser.Stream.CanSeek)
+            {
+                // make copy of data from stream
+                var builder = new BlobBuilder();
+                builder.AddUInt32(header.BlobLength);
+                builder.AddUInt8((byte)header.Message);
+                builder.AddBytes(messageParser.ReadBytes(header.BlobLength));
+
+                // replace the parser with the in-memory stream
+                messageParser = new BlobParser(builder.GetBlob());
+
+                // ensure the parser is in the same position it was previously
+                messageParser.ReadHeader();
+            }
+
             switch (header.Message)
             {
                 case Message.SSH1_AGENTC_REQUEST_RSA_IDENTITIES:
@@ -698,7 +716,7 @@ namespace dlech.SshAgentLib
 
                         if (ssh1constrained)
                         {
-                            while (messageStream.Position < header.BlobLength + 4)
+                            while (messageParser.Stream.Position < header.BlobLength + 4)
                             {
                                 var constraint = new KeyConstraint
                                 {
@@ -760,7 +778,7 @@ namespace dlech.SshAgentLib
 
                         if (constrained)
                         {
-                            while (messageStream.Position < header.BlobLength + 4)
+                            while (messageParser.Stream.Position < header.BlobLength + 4)
                             {
                                 KeyConstraint constraint = new KeyConstraint
                                 {

SshAgentLibTests/AgentClientTest.cs
@@ -1,61 +1,40 @@
-﻿//
-// AgentClientTest.cs
-//
-// Author(s): David Lechner <david@lechnology.com>
-//
-// Copyright (c) 2012-2013,2015,2017,2022 David Lechner
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+﻿// SPDX-License-Identifier: MIT
+// Copyright (c) 2012-2013,2015,2017,2022 David Lechner <david@lechnology.com>
 
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
 using dlech.SshAgentLib;
+using dlech.SshAgentLibTests;
 using NUnit.Framework;
 using Org.BouncyCastle.Asn1;
 using Org.BouncyCastle.Math;
 
-namespace dlech.SshAgentLibTests
+namespace SshAgentLibTests
 {
-    [TestFixture]
-    public class AgentClientTest
+    [TestFixture(typeof(MemoryStream))]
+    [TestFixture(typeof(NonSeekableMemoryStream))]
+    public class AgentClientTest<TStream> where TStream : MemoryStream
     {
-        static SshKey rsa1Key,
-            rsaKey,
-            rsaCert,
-            dsaKey,
-            dsaCert,
-            ecdsa256Key,
-            ecdsa256Cert,
-            ecdsa384Key,
-            ecdsa384Cert,
-            ecdsa521Key,
-            ecdsa521Cert,
-            ed25519Key,
-            ed25519Cert;
-        static ReadOnlyCollection<SshKey> allKeys;
+        private static readonly SshKey rsa1Key;
+        private static readonly SshKey rsaKey;
+        private static readonly SshKey rsaCert;
+        private static readonly SshKey dsaKey;
+        private static readonly SshKey dsaCert;
+        private static readonly SshKey ecdsa256Key;
+        private static readonly SshKey ecdsa256Cert;
+        private static readonly SshKey ecdsa384Key;
+        private static readonly SshKey ecdsa384Cert;
+        private static readonly SshKey ecdsa521Key;
+        private static readonly SshKey ecdsa521Cert;
+        private static readonly SshKey ed25519Key;
+        private static readonly SshKey ed25519Cert;
+        private static readonly ReadOnlyCollection<SshKey> allKeys;
 
         class TestAgentClient : AgentClient
         {
@@ -70,11 +49,24 @@ namespace dlech.SshAgentLibTests
             {
                 var buffer = new byte[4096];
                 Array.Copy(message, buffer, message.Length);
-                var messageStream = new MemoryStream(buffer);
+                var messageStream = (TStream)Activator.CreateInstance(typeof(TStream), buffer);
+
                 Agent.AnswerMessage(messageStream);
-                var reply = new byte[messageStream.Position];
-                Array.Copy(buffer, reply, reply.Length);
-                return reply;
+
+                // If the stream is seekable, it should have been rewound and
+                // the reply written to the start of the buffer.
+                if (messageStream.CanSeek)
+                {
+                    return buffer;
+                }
+
+                // Otherwise the reply is written to the buffer immediately
+                // after the message.
+                return new ArraySegment<byte>(
+                    buffer,
+                    message.Length + 1,
+                    buffer.Length - message.Length - 1
+                ).ToArray();
             }
         }
 
@@ -151,20 +143,23 @@ namespace dlech.SshAgentLibTests
                 "SSH2 Ed25519 test key + cert"
             );
 
-            List<SshKey> keyList = new List<SshKey>();
-            keyList.Add(rsa1Key);
-            keyList.Add(rsaKey);
-            keyList.Add(rsaCert);
-            keyList.Add(dsaKey);
-            keyList.Add(dsaCert);
-            keyList.Add(ecdsa256Key);
-            keyList.Add(ecdsa256Cert);
-            keyList.Add(ecdsa384Key);
-            keyList.Add(ecdsa384Cert);
-            keyList.Add(ecdsa521Key);
-            keyList.Add(ecdsa521Cert);
-            keyList.Add(ed25519Key);
-            keyList.Add(ed25519Cert);
+            var keyList = new List<SshKey>
+            {
+                rsa1Key,
+                rsaKey,
+                rsaCert,
+                dsaKey,
+                dsaCert,
+                ecdsa256Key,
+                ecdsa256Cert,
+                ecdsa384Key,
+                ecdsa384Cert,
+                ecdsa521Key,
+                ecdsa521Cert,
+                ed25519Key,
+                ed25519Cert
+            };
+
             allKeys = keyList.AsReadOnly();
         }
 
@@ -172,17 +167,19 @@ namespace dlech.SshAgentLibTests
         public void TestAddConstrainedKey()
         {
             var agentClient = new TestAgentClient();
-            agentClient.Agent.ConfirmUserPermissionCallback = delegate
-            {
-                return true;
-            };
+            agentClient.Agent.ConfirmUserPermissionCallback = (k, p) => true;
+
             Agent.KeyConstraint constraint;
             List<Agent.KeyConstraint> constraints = new List<Agent.KeyConstraint>();
 
-            constraint = new Agent.KeyConstraint();
-            constraint.Type = Agent.KeyConstraintType.SSH_AGENT_CONSTRAIN_CONFIRM;
+            constraint = new Agent.KeyConstraint
+            {
+                Type = Agent.KeyConstraintType.SSH_AGENT_CONSTRAIN_CONFIRM
+            };
+
             constraints.Add(constraint);
             agentClient.AddKey(rsaKey, constraints);
+
             Assert.That(agentClient.Agent.KeyCount, Is.EqualTo(1));
             Assert.That(agentClient.Agent.GetAllKeys().First().Constraints.Count, Is.EqualTo(1));
             Assert.That(
@@ -190,12 +187,16 @@ namespace dlech.SshAgentLibTests
                 Is.EqualTo(Agent.KeyConstraintType.SSH_AGENT_CONSTRAIN_CONFIRM)
             );
 
-            constraint = new Agent.KeyConstraint();
-            constraint.Type = Agent.KeyConstraintType.SSH_AGENT_CONSTRAIN_LIFETIME;
-            constraint.Data = (uint)10;
+            constraint = new Agent.KeyConstraint
+            {
+                Type = Agent.KeyConstraintType.SSH_AGENT_CONSTRAIN_LIFETIME,
+                Data = (uint)10
+            };
+
             constraints.Clear();
             constraints.Add(constraint);
             agentClient.AddKey(rsaKey, constraints);
+
             Assert.That(agentClient.Agent.KeyCount, Is.EqualTo(1));
             Assert.That(agentClient.Agent.GetAllKeys().First().Constraints.Count, Is.EqualTo(1));
             Assert.That(
@@ -324,8 +325,8 @@ namespace dlech.SshAgentLibTests
                             var md5Buffer = new byte[48];
                             data.CopyTo(md5Buffer, 0);
                             agentClient.SessionId.CopyTo(md5Buffer, 32);
-                            var expctedSignature = md5.ComputeHash(md5Buffer);
-                            Assert.That(signature, Is.EqualTo(expctedSignature));
+                            var expectedSignature = md5.ComputeHash(md5Buffer);
+                            Assert.That(signature, Is.EqualTo(expectedSignature));
                         }
                         break;
                     case SshVersion.SSH2:
@@ -397,15 +398,47 @@ namespace dlech.SshAgentLibTests
             );
 
             /* try with null passphrase */
-            Assert.That(() => agentClient.Lock(null), Throws.Nothing);
-            Assert.That(() => agentClient.Unlock(null), Throws.Nothing);
+            Assert.That(() => agentClient.Lock(null), Throws.ArgumentNullException);
+            Assert.That(() => agentClient.Unlock(null), Throws.ArgumentNullException);
 
             /* verify that bad passphrase fails */
             Assert.That(() => agentClient.Lock(passphrase), Throws.Nothing);
             Assert.That(
-                () => agentClient.Unlock(null),
+                () => agentClient.Unlock(Array.Empty<byte>()),
                 Throws.Exception.TypeOf<AgentFailureException>()
             );
+        }
+    }
+
+    /// <summary>
+    /// Memory stream with seek function disabled.
+    /// </summary>
+    /// <remarks>
+    /// This is used to simulate an agent that uses a network stream
+    /// that doesn't support seeking.
+    /// </remarks>
+    internal class NonSeekableMemoryStream : MemoryStream
+    {
+        public NonSeekableMemoryStream(byte[] buffer) : base(buffer) { }
+
+        public override bool CanSeek => false;
+
+        public override long Length => throw new NotSupportedException();
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override long Position
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin loc)
+        {
+            throw new NotSupportedException();
         }
     }
 }

SshAgentLib/AgentClient.cs
@@ -1,4 +1,4 @@
-﻿//
+//
 // AgentClient.cs
 //
 // Author(s): David Lechner <david@lechnology.com>
@@ -296,24 +296,28 @@ namespace dlech.SshAgentLib
             }
         }
 
-        public void Lock(byte[] aPassphrase)
+        public void Lock(byte[] passphrase)
         {
-            BlobBuilder builder = new BlobBuilder();
-            if (aPassphrase != null)
+            if (passphrase is null)
             {
-                builder.AddBlob(aPassphrase);
+                throw new ArgumentNullException(nameof(passphrase));
             }
+
+            BlobBuilder builder = new BlobBuilder();
+            builder.AddBlob(passphrase);
             builder.InsertHeader(Agent.Message.SSH_AGENTC_LOCK);
             SendMessageAndCheckSuccess(builder);
         }
 
-        public void Unlock(byte[] aPassphrase)
+        public void Unlock(byte[] passphrase)
         {
-            BlobBuilder builder = new BlobBuilder();
-            if (aPassphrase != null)
+            if (passphrase is null)
             {
-                builder.AddBlob(aPassphrase);
+                throw new ArgumentNullException(nameof(passphrase));
             }
+
+            BlobBuilder builder = new BlobBuilder();
+            builder.AddBlob(passphrase);
             builder.InsertHeader(Agent.Message.SSH_AGENTC_UNLOCK);
             SendMessageAndCheckSuccess(builder);
         }

SshAgentLib/BlobParser.cs
@@ -50,78 +50,55 @@ namespace dlech.SshAgentLib
 
         public BlobParser(Stream stream)
         {
-            if (stream == null)
-            {
-                throw new ArgumentNullException("stream");
-            }
-            Stream = stream;
+            Stream = stream ?? throw new ArgumentNullException(nameof(stream));
         }
 
         public byte ReadUInt8()
         {
-            if (Stream.CanSeek && Stream.Length - Stream.Position < 1)
-            {
-                throw new Exception("Not enough data");
-            }
             return (byte)Stream.ReadByte();
         }
 
         public ushort ReadUInt16()
         {
-            byte[] dataLegthBytes = new byte[2];
-            if (Stream.CanSeek && Stream.Length - Stream.Position < dataLegthBytes.Length)
-            {
-                throw new Exception("Not enough data");
-            }
-            Stream.Read(dataLegthBytes, 0, dataLegthBytes.Length);
-            return (ushort)((dataLegthBytes[0] << 8) | dataLegthBytes[1]);
+            byte[] dataLengthBytes = new byte[2];
+            Stream.Read(dataLengthBytes, 0, dataLengthBytes.Length);
+            return (ushort)((dataLengthBytes[0] << 8) | dataLengthBytes[1]);
         }
 
         public uint ReadUInt32()
         {
-            byte[] dataLegthBytes = new byte[4];
-            if (Stream.CanSeek && Stream.Length - Stream.Position < dataLegthBytes.Length)
-            {
-                throw new Exception("Not enough data");
-            }
-            Stream.Read(dataLegthBytes, 0, dataLegthBytes.Length);
+            byte[] dataLengthBytes = new byte[4];
+            Stream.Read(dataLengthBytes, 0, dataLengthBytes.Length);
             return (uint)(
-                (dataLegthBytes[0] << 24)
-                | (dataLegthBytes[1] << 16)
-                | (dataLegthBytes[2] << 8)
-                | dataLegthBytes[3]
+                (dataLengthBytes[0] << 24)
+                | (dataLengthBytes[1] << 16)
+                | (dataLengthBytes[2] << 8)
+                | dataLengthBytes[3]
             );
         }
 
         public ulong ReadUInt64()
         {
-            byte[] dataLegthBytes = new byte[8];
-            if (Stream.CanSeek && Stream.Length - Stream.Position < dataLegthBytes.Length)
-            {
-                throw new Exception("Not enough data");
-            }
-            Stream.Read(dataLegthBytes, 0, dataLegthBytes.Length);
+            byte[] dataLengthBytes = new byte[8];
+            Stream.Read(dataLengthBytes, 0, dataLengthBytes.Length);
             return (ulong)(
-                (dataLegthBytes[0] << 56)
-                | (dataLegthBytes[1] << 48)
-                | (dataLegthBytes[2] << 40)
-                | (dataLegthBytes[3] << 32) + (dataLegthBytes[4] << 24)
-                | (dataLegthBytes[5] << 16)
-                | (dataLegthBytes[6] << 8)
-                | dataLegthBytes[7]
+                (dataLengthBytes[0] << 56)
+                | (dataLengthBytes[1] << 48)
+                | (dataLengthBytes[2] << 40)
+                | (dataLengthBytes[3] << 32) + (dataLengthBytes[4] << 24)
+                | (dataLengthBytes[5] << 16)
+                | (dataLengthBytes[6] << 8)
+                | dataLengthBytes[7]
             );
         }
 
         public Agent.BlobHeader ReadHeader()
         {
-            Agent.BlobHeader header = new Agent.BlobHeader();
-
-            header.BlobLength = ReadUInt32();
-            if (Stream.CanSeek && Stream.Length - Stream.Position < header.BlobLength)
+            Agent.BlobHeader header = new Agent.BlobHeader
             {
-                throw new Exception("Not enough data");
-            }
-            header.Message = (Agent.Message)ReadUInt8();
+                BlobLength = ReadUInt32(),
+                Message = (Agent.Message)ReadUInt8()
+            };
             return header;
         }
 
@@ -148,10 +125,6 @@ namespace dlech.SshAgentLib
 
         public byte[] ReadBytes(uint blobLength)
         {
-            if (Stream.CanSeek && Stream.Length - Stream.Position < blobLength)
-            {
-                throw new Exception("Not enough data");
-            }
             var blob = new byte[(int)blobLength];
             Stream.Read(blob, 0, blob.Length);
             return blob;
@@ -193,8 +166,7 @@ namespace dlech.SshAgentLib
             var validBeforeDateTime =
                 validBefore == ulong.MaxValue ? DateTime.MaxValue : epoch.AddSeconds(validBefore);
             var signatureKeyParser = new BlobParser(signatureKey);
-            OpensshCertificate unused;
-            var sigKey = signatureKeyParser.ReadSsh2PublicKeyData(out unused);
+            var sigKey = signatureKeyParser.ReadSsh2PublicKeyData(out _);
 
             return new OpensshCertificate(
                 builder.GetBlob(),
@@ -492,10 +464,7 @@ namespace dlech.SshAgentLib
         /// <returns>AsymmetricKeyParameter containing the public key</returns>
         public AsymmetricKeyParameter ReadSsh1PublicKeyData(bool reverseRsaParameters = false)
         {
-            // ignore not used warning
-#pragma warning disable 0219
-            uint keyLength = ReadUInt32();
-#pragma warning restore 0219
+            _ = ReadUInt32(); // key_bits - unused
             var rsaN = new BigInteger(1, ReadSsh1BigIntBlob());
             var rsaE = new BigInteger(1, ReadSsh1BigIntBlob());
 

commit 192c5fd49ab309b25ce1c444208afc3dd1d2a4e2

Agent: fix off-by-one error
This error was introduced in 0980bee3caaa012ea6887bf4c324642f5089dffe.

SshAgentLib/Agent.cs
@@ -449,7 +449,7 @@ namespace dlech.SshAgentLib
                 var builder = new BlobBuilder();
                 builder.AddUInt32(header.BlobLength);
                 builder.AddUInt8((byte)header.Message);
-                builder.AddBytes(messageParser.ReadBytes(header.BlobLength));
+                builder.AddBytes(messageParser.ReadBytes(header.BlobLength -1));
 
                 // replace the parser with the in-memory stream
                 messageParser = new BlobParser(builder.GetBlob());

SshAgentLibTests/AgentClientTest.cs
@@ -64,8 +64,8 @@ namespace SshAgentLibTests
                 // after the message.
                 return new ArraySegment<byte>(
                     buffer,
-                    message.Length + 1,
-                    buffer.Length - message.Length - 1
+                    message.Length,
+                    buffer.Length - message.Length
                 ).ToArray();
             }
         }