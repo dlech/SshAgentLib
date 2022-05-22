48bab01e357d968202d6caca95f4702b13f356f3
David Lechner <david@lechnology.com>
2022-05-22 16:41:43 -0500

    scripts/nunit: add --noresult option
    This suppresses the output of the result .xml file when tests are run.
    
    .log files are still created due to https://github.com/nunit/nunit-console/issues/1178.

--- scripts/run-tests-mono.sh
+++ scripts/run-tests-mono.sh
@@ -4,4 +4,4 @@
 set -e
 
 xbuild SshAgentLibTests/SshAgentLibTests.csproj
-mono ./packages/NUnit.ConsoleRunner/tools/nunit3-console.exe SshAgentLibTests/bin/Debug/SshAgentLibTests.dll --framework=mono-4.0 "$@"
+mono ./packages/NUnit.ConsoleRunner/tools/nunit3-console.exe SshAgentLibTests/bin/Debug/SshAgentLibTests.dll --framework=mono-4.0 --noresult "$@"
